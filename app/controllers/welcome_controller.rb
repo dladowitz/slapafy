968d0fd2c4e4d63bef0cdad97a878452c3b3c3d1
David Ladowitz <david@ladowitz.com>
2016-01-31 22:16:48 -0800

    adding more analytics things"

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,27 +1,37 @@
+# https://developers.google.com/api-client-library/ruby/auth/web-app
+
+require 'google/apis/drive_v2'
+require 'google/apis/analytics_v3'
 require 'google/api_client/client_secrets'
 
 class WelcomeController < ApplicationController
   def landing
-    authenticate
+    return redirect_to "/oauthredirect" unless session["google-auth-client"]
+
+    client_opts = JSON.parse(session["google-auth-client"])
+    auth_client = Signet::OAuth2::Client.new(client_opts)
+
+    analytics = Google::Apis::AnalyticsV3::AnalyticsService.new
+    @results = analytics.get_ga_data('ga:103055258', '7daysAgo', 'yesterday', 'ga:newusers,ga:transactions,ga:transactionRevenue,ga:goal4Completions', dimensions: 'ga:source', filters: 'ga:source==CurlyByNature', options:{ authorization: auth_client })
   end
 
   def oauthredirect
-    
-  end
-
-
-  private
-
-  def authenticate
     client_secrets = Google::APIClient::ClientSecrets.load('google-web.json')
     auth_client = client_secrets.to_authorization
     auth_client.update!(
-      :scope => 'https://www.googleapis.com/auth/drive.metadata.readonly',
+      :scope => 'https://www.googleapis.com/auth/analytics.readonly',
       :redirect_uri => 'http://localhost:3000/oauthredirect'
     )
 
-    auth_uri = auth_client.authorization_uri.to_s
-
-    redirect_to auth_uri
+    if params["code"] 
+      auth_client.code = params["code"]
+      auth_client.fetch_access_token!
+      auth_client.client_secret = nil # not sure why this is
+      session["google-auth-client"] = auth_client.to_json
+      return redirect_to root_path
+    else
+      google_auth_uri = auth_client.authorization_uri.to_s
+      return redirect_to google_auth_uri
+    end
   end
 end
