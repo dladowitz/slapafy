a59768916fd73b2369f3776ef0f12c71d939d8aa
David Ladowitz <david@ladowitz.com>
2016-02-01 21:38:16 -0800

    fixing json file problem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,11 @@
 # https://developers.google.com/api-client-library/ruby/auth/web-app
-
 require 'google/apis/analytics_v3'
 require 'google/api_client/client_secrets'
 
 class WelcomeController < ApplicationController
-  def landing
-    return redirect_to "/oauthredirect" #unless session["google-auth-client"]
+  before_action :authorize_ga_api, except: :oauthredirect
 
+  def landing
     client_opts = JSON.parse(session["google-auth-client"])
     auth_client = Signet::OAuth2::Client.new(client_opts)
 
@@ -15,8 +14,7 @@ class WelcomeController < ApplicationController
   end
 
   def oauthredirect
-    binding.pry
-    client_secrets = Google::APIClient::ClientSecrets.load('google-web.json')
+    client_secrets = Google::APIClient::ClientSecrets.new(JSON.parse(ENV['GOOGLE_CLIENT_SECRETS']))
     auth_client = client_secrets.to_authorization
     auth_client.update!(
       :scope => 'https://www.googleapis.com/auth/analytics.readonly',
