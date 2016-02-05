9ef57d10dab21e861d29e6c5b1fcba155bfe58a5
David Ladowitz <david@ladowitz.com>
2016-02-05 00:12:04 -0800

    fixing typo

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -44,7 +44,7 @@ class ReportsController < ApplicationController
       youtube   = youtube_stats(video)
       analytics = google_analytics_stats(video)
 
-      if analytics.totals_for_all_results do 
+      if analytics.totals_for_all_results
         video.stats.create({report_id: @report.id, 
                             views: youtube[:view_count], 
                             channel_subscribers: youtube[:subscriber_count], 
